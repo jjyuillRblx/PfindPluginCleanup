222c85457388c53dbf7cbcb780f2f21a078c58e0
jjyuillRblx <111813971+jjyuillRblx@users.noreply.github.com>
2024-06-17 09:05:49 -0400

    Update DialogLib.lua

--- DialogLib.lua
+++ DialogLib.lua
@@ -1,113 +1,58 @@
-local gm = {}
+local Module = { }
+local Parts = { }
 
-
-local coreGui = game:GetService("CoreGui")
-local root = script.Parent.Guis
-local sgui = Instance.new("ScreenGui", coreGui)
-
-
-
-local function c3(r, g, b)
-	return Color3.new(r/255, g/255, b/255)
-end
-
-
-function gm.DrawLine(pos1, pos2)
-	local ignore = workspace:findFirstChild("Ignore")
-	if not ignore then
-		ignore = Instance.new("Model", workspace)
-		ignore.Name = "Ignore"
+function Module.DestroyLines()
+	for	_, Part in Parts do
+		Part:Destroy()
 	end
 	
-	local dist = (pos1-pos2).magnitude
-	local rayPart = Instance.new("Part", workspace.Ignore)
-	rayPart.Name = "NodeConnector"
-	rayPart.BrickColor = BrickColor.new("Lime green")
-	rayPart.Transparency = 0.5
-	rayPart.Anchored = true
-	rayPart.CanCollide = false
-	rayPart.TopSurface = Enum.SurfaceType.Smooth
-	rayPart.BottomSurface = Enum.SurfaceType.Smooth
-	rayPart.formFactor = Enum.FormFactor.Custom
-	rayPart.Size = Vector3.new(0.2, 0.2, dist)
-	rayPart.CFrame = CFrame.new(pos1, pos2) * CFrame.new(0, 0, -dist/2)
-	rayPart.Archivable = false
-	return rayPart
+	Parts = { }
 end
 
-
-
-function gm.Dialog(text, dur, dtype)
-	local dur = dur or 2
-	local dg
-	if dtype ~= nil then
-		dg = root["Dialog"..dtype]:clone()
-	else
-		dg = root["Dialog"]:clone()
-	end
-	print(text)
-	dg.TextLabel.Text = text
+function Module.DrawLine(Pos1, Pos2, Width)
+	local Ignore = Module.Ignore
 	
-	local origin = dg.Position
-	dg.Position = UDim2.new(0.4, 0, -0.4, 0)
-	dg.Parent = sgui
-	dg:TweenPosition(origin, Enum.EasingDirection.Out, Enum.EasingStyle.Quad, 0.5)
-	
-	if dtype == "YesNo" or dtype == "OkCancel" then
-		local yes, no = false, false
-		dg.YesButton.MouseButton1Click:connect(function()
-			yes = true
-		end)
-		dg.NoButton.MouseButton1Click:connect(function()
-			no = true
-		end)
-		repeat
-			wait()
-		until yes or no
-		dg:TweenPosition(UDim2.new(0.4, 0, -0.4, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quad, 0.5)
-		if yes then
-			return true
-		else
-			return false
-		end
-	else
-		spawn(function()
-			wait(dur)
-			dg:TweenPosition(UDim2.new(0.4, 0, -0.4, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quad, 0.5)
-		end)
+	if not Ignore then
+		Ignore = Instance.new("Folder")
+		Ignore.Name = "Ignore"
+		Ignore.Parent = workspace
+		
+		Module.Ignore = Ignore
 	end
+	
+	local Distance = (Pos1 - Pos2).Magnitude
+	local RayPart = script.NodeConnector:Clone()
+	RayPart.Size = Vector3.new(Width or 0.2, 0.2, Distance)
+	RayPart.CFrame = CFrame.new(Pos1, Pos2) * CFrame.new(0, 0, -Distance / 2)
+	RayPart.Archivable = false
+	
+	RayPart.Parent = Ignore
+	
+	return RayPart
 end
 
-
-
-function gm.ProgressBar(text, pct)
-	local pb = root.DialogProgressbar:clone()
+function Module.ProgressBar(Start)
+	local Gui = script.Progress:Clone()
+	Gui = game:GetService("CoreGui")
 	
-	local origin = pb.Position
-	pb.Position = UDim2.new(0.4, 0, -0.4, 0)
-	pb.Parent = sgui
-	pb.TextLabel.Text = text
-	pb:TweenPosition(origin, Enum.EasingDirection.Out, Enum.EasingStyle.Quad, 0.5)
+	local Label = Gui.Main.TextLabel
+	local Progress = Gui.Main.Progress
+	
+	Progress.Size = UDim2.new(Start / 100, 0, 1, 0)
 	
 	return {
-		instance = pb.ProgressFrame.ProgressBar;
-		percentage = 0;
-		Update = function(self, pct)
-			self.instance:TweenSize(UDim2.new(pct / 100, 0, 1, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quad, 0.25)
-			self.instance.Parent.Percentage.Text = math.floor(pct) .. " %"
+		Update = function(Percent)
+			Progress.Size = UDim2.new(Percent, 0, 1, 0)
 		end;
-		TextUpdate = function(self, text)
-			self.instance.Parent.Parent.TextLabel.Text = text
+		TextUpdate = function(Text)
+			Label.Text = Text
 		end;
-		Kill = function(self)
-			self.instance.Parent.Parent:TweenPosition(UDim2.new(0.4, 0, -0.4, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quad, 0.5)
-			wait(0.6)
-			self.instance.Parent.Parent:Destroy()
-		end;
+		Kill = function(Time)
+			task.wait(Time or 0)
+			
+			Gui:Destroy()
+		end
 	}
 end
 
-
-
-return gm
-
+return Module
